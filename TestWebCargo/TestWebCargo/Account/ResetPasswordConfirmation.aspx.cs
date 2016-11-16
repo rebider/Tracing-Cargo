48a49e3c2ea2d4f658b2eb48ed5f28ea1bc16dd2
chenyuanyile <2389154518@qq.com>
2016-11-16 15:17:25 +0800

    Test Commit

---
+++ TestWebCargo/TestWebCargo/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace TestWebCargo.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ TestWebCargo/TestWebCargo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestWebCargo.Startup))]
+namespace TestWebCargo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
